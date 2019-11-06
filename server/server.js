386f15bffa92df6819442371435461d271e96f8a
yassine belkaid <yassine.belkaid87@gmail.com>
2019-11-06 16:25:24 +0100

    [server] setup the post login route

--- server/server.js
+++ server/server.js
@@ -1,14 +1,36 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const expressJWT = require("express-jwt");
+const jwt = require("jsonwebtoken");
+
+const db = require("./db");
 
 const app = express();
 const PORT = 5000;
+const jwtSecret = Buffer.from("somelongsecrettokenhere", "base64");
 
-app.use(cors(), bodyParser.json());
+app.use(
+  cors(),
+  bodyParser.json(),
+  expressJWT({
+    secret: jwtSecret,
+    credentialsRequired: false
+  })
+);
 
-app.get("/", (req, res) => {
-  res.send("Hello there");
+app.post("/login", (req, res) => {
+  const { email, password } = req.body;
+  const user = db.users.get(email);
+
+  if (!(user && user.password === password)) {
+    res.sendStatus(401);
+    return;
+  }
+
+  const token = jwt.sign({ user: user.id }, jwtSecret);
+
+  res.send({ token });
 });
 
 app.listen(PORT, () => console.log(`app running on port ${PORT}`));
