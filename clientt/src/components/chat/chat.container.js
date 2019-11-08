283fbb7c1bf3615ce80c7040b7dfed1e53a1fba1
yassine belkaid <yassine.belkaid87@gmail.com>
2019-11-08 10:16:30 +0100

    [client] modified chat container to add fake messages to test the workflow

--- clientt/src/components/chat/chat.container.js
+++ clientt/src/components/chat/chat.container.js
@@ -1,12 +1,39 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 
 import Chat from "./chat.component";
 import { AuthContext } from "../../context";
 
+const KEYBOARD_ENTER_NUM = 13;
+
 const ChatContanier = () => {
   const authContext = useContext(AuthContext);
+  const [messages, setMessages] = useState([]);
+  const [userId, setUserId] = useState(1);
 
-  return <Chat isLoggedin={authContext[0]} />;
+  const onKeyPressHanlder = e => {
+    const message = e.target.value;
+
+    if (e.which === KEYBOARD_ENTER_NUM && message) {
+      setMessages(
+        messages.concat({
+          id: userId,
+          user: "yassinne",
+          message
+        })
+      );
+      setUserId(id => id + 1);
+      e.target.value = "";
+    }
+  };
+
+  return (
+    <Chat
+      isLoggedin={authContext[0]}
+      messages={messages}
+      user="yassine"
+      onKeyPress={onKeyPressHanlder}
+    />
+  );
 };
 
 export default ChatContanier;
