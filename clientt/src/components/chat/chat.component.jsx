b2510b918a33644cc38088ff7502a5dabbb75a00
yassine belkaid <yassine.belkaid87@gmail.com>
2019-11-08 10:15:46 +0100

    [client] modified main chat component by adding chidlren components

--- clientt/src/components/chat/chat.component.jsx
+++ clientt/src/components/chat/chat.component.jsx
@@ -1,9 +1,21 @@
 import React from "react";
 
-const Chat = ({ isLoggedin }) => {
+import ChatInput from "./chat-input/chat-input.component";
+import ChatMessages from "./chat-messages/chat-messages.component";
+import styles from "./chat.module.css";
+
+const Chat = ({ isLoggedin, user, onKeyPress, messages }) => {
   if (!isLoggedin) return null;
 
-  return <div>Chat room</div>;
+  return (
+    <section className={styles.Chat}>
+      <div className={styles.ChatHeader}>
+        Logged in as: <h3>{user}</h3>
+      </div>
+      <ChatMessages messages={messages} />
+      <ChatInput onKeyPress={onKeyPress} />
+    </section>
+  );
 };
 
 export default Chat;
