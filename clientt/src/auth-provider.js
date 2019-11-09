815510137c24630b0e7f2d5c21fccf040320e316
yassine belkaid <yassine.belkaid87@gmail.com>
2019-11-09 10:06:43 +0100

    [client] refactored auth provider to track current user object from token and pass it down tree

--- clientt/src/auth-provider.js
+++ clientt/src/auth-provider.js
@@ -1,11 +1,28 @@
 import React, { useState, useMemo } from "react";
 
 import { AuthContext } from "./context";
-import { getToken } from "./storage";
+import { getToken, getUserFromToken } from "./storage";
 
 const AuthProvider = ({ children }) => {
   const [isLogged, setIsLogged] = useState(Boolean(getToken()));
-  const providerData = useMemo(() => [isLogged, setIsLogged], [isLogged]);
+  const [currentUser, setCurrentUser] = useState(null);
+  const providerData = useMemo(
+    () => ({
+      isLogged,
+      setIsLogged,
+      currentUser,
+      setCurrentUser
+    }),
+    [isLogged, currentUser]
+  );
+
+  React.useEffect(() => {
+    const token = getUserFromToken();
+
+    if (token) {
+      setCurrentUser(token);
+    }
+  }, []);
 
   return (
     <AuthContext.Provider value={providerData}>{children}</AuthContext.Provider>
