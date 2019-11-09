1e9d163ef5384e19d7c5442b5e6f431b54a9819b
yassine belkaid <yassine.belkaid87@gmail.com>
2019-11-09 09:05:28 +0100

    [server] added addMessage mutation to resolvers

--- server/graphql/resolvers.js
+++ server/graphql/resolvers.js
@@ -4,6 +4,16 @@ const Query = {
   messages: () => messages.list()
 };
 
+const Mutation = {
+  addMessage: (_, { user, message }) => {
+    const createData = { user, message };
+    const messageId = messages.create(createData);
+
+    return { id: messageId, ...createData };
+  }
+};
+
 module.exports = {
-  Query
+  Query,
+  Mutation
 };
