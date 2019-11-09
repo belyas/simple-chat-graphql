1a18172de3a418f37cc3b5d55be0a5f316559561
yassine belkaid <yassine.belkaid87@gmail.com>
2019-11-09 17:17:48 +0100

    [server] added subscription messageAdded method to resolvers and publish its event on addMessage mutation

--- server/graphql/resolvers.js
+++ server/graphql/resolvers.js
@@ -1,4 +1,9 @@
+const { PubSub } = require("graphql-subscriptions");
+
 const { messages } = require("../db");
+
+const pubsub = new PubSub();
+const MESSAGE_ADDED = "MESSAGE_ADDED";
 
 const Query = {
   messages: () => messages.list()
@@ -8,12 +13,22 @@ const Mutation = {
   addMessage: (_, { user, message }) => {
     const createData = { user, message };
     const messageId = messages.create(createData);
+    const newMessage = { id: messageId, ...createData };
 
-    return { id: messageId, ...createData };
+    pubsub.publish(MESSAGE_ADDED, { messageAdded: newMessage });
+
+    return newMessage;
+  }
+};
+
+const Subscription = {
+  messageAdded: {
+    subscribe: () => pubsub.asyncIterator([MESSAGE_ADDED])
   }
 };
 
 module.exports = {
   Query,
-  Mutation
+  Mutation,
+  Subscription
 };
