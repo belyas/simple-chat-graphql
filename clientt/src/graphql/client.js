5dcb313d911f1465afeb5e67115d452544b34fa5
yassine belkaid <yassine.belkaid87@gmail.com>
2019-11-09 17:18:35 +0100

    [client] added ws link to graphql client

--- clientt/src/graphql/client.js
+++ clientt/src/graphql/client.js
@@ -1,9 +1,33 @@
-import { ApolloClient, InMemoryCache, HttpLink } from "apollo-boost";
+import { ApolloClient, InMemoryCache, HttpLink, split } from "apollo-boost";
+import { WebSocketLink } from "apollo-link-ws";
+import { getMainDefinition } from "apollo-utilities";
 
 const SERVER_URL = "http://localhost:5000/graphql";
+const SERVER_WS_URL = "ws://localhost:5000/graphql";
+
+const wsLink = new WebSocketLink({
+  uri: SERVER_WS_URL,
+  options: {
+    reconnect: true,
+    lazy: true
+  }
+});
+
+const httpLink = new HttpLink({ uri: SERVER_URL });
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query);
+
+    return kind === "OperationDefinition" && operation === "subscription";
+  },
+  wsLink,
+  httpLink
+);
+
 const client = new ApolloClient({
-  link: new HttpLink({ uri: SERVER_URL }),
-  cache: new InMemoryCache()
+  link,
+  cache: new InMemoryCache(),
+  defaultOptions: { query: { fetchPolicy: "no-cache" } }
 });
 
 export default client;
