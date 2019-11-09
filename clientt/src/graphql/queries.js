fba276fdae631c95f13753ce9edaebf3ef4902b3
yassine belkaid <yassine.belkaid87@gmail.com>
2019-11-09 10:23:56 +0100

    [client] added add message mutation query

--- clientt/src/graphql/queries.js
+++ clientt/src/graphql/queries.js
@@ -9,3 +9,13 @@ export const MESSAGES_LIST = gql`
     }
   }
 `;
+
+export const ADD_MESSAGE = gql`
+  mutation addMessageMutation($user: String!, $message: String!) {
+    addMessage(user: $user, message: $message) {
+      id
+      user
+      message
+    }
+  }
+`;
