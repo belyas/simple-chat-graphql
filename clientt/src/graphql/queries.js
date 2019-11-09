52819191980eb75658812cad3bab5baa8c14b882
yassine belkaid <yassine.belkaid87@gmail.com>
2019-11-09 17:19:27 +0100

    [client] added messageAdded query and onMessageAdded function to async the given handler

--- clientt/src/graphql/queries.js
+++ clientt/src/graphql/queries.js
@@ -1,5 +1,7 @@
 import { gql } from "apollo-boost";
 
+import client from "./client";
+
 export const MESSAGES_LIST = gql`
   query MessagesQuery {
     messages {
@@ -19,3 +21,27 @@ export const ADD_MESSAGE = gql`
     }
   }
 `;
+
+const MESSAGE_ADDED = gql`
+  subscription onMessagedAdded {
+    messageAdded {
+      id
+      user
+      message
+    }
+  }
+`;
+
+export const getMessages = async () => {
+  const { data } = await client.query({ query: MESSAGES_LIST });
+
+  return data.messages;
+};
+
+export const onMessagedAdded = messageCallback => {
+  const observable = client.subscribe({ query: MESSAGE_ADDED });
+
+  return observable.subscribe(({ data }) => {
+    messageCallback(data.messageAdded);
+  });
+};
